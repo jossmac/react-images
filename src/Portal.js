aebd7599e6fe238454c656bede5d7e8aefcc04a9
Joss Mackison <joss@thinkmill.com.au>
2016-05-16 12:49:37 +1000

    remove unused node when rendering portal/transition
    [Rendering a Single
    Child](https://facebook.github.io/react/docs/animation.html#rendering-a-
    single-child)

--- src/Portal.js
+++ src/Portal.js
@@ -1,6 +1,11 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Children, Component, PropTypes } from 'react';
 import Transition from 'react-addons-transition-group';
 import { render } from 'react-dom';
+
+const FirstChild = ({ children }) => {
+	let kids = Children.toArray(children);
+	return kids[0] || null;
+};
 
 export default class Portal extends Component {
 	constructor () {
@@ -13,23 +18,20 @@ export default class Portal extends Component {
 		this.portalElement = p;
 		this.componentDidUpdate();
 	}
-
 	componentDidUpdate () {
 		render(
-			<Transition {...this.props} component="div">{this.props.children}</Transition>,
+			<Transition {...this.props} component={FirstChild} />,
 				this.portalElement
 		);
 	}
-
 	componentWillUnmount () {
 		document.body.removeChild(this.portalElement);
 	}
-
 	render () {
 		return null;
 	}
 }
 
 Portal.propTypes = {
-	children: PropTypes.any,
+	children: PropTypes.element,
 };
