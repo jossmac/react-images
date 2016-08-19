f96be06ca99403eabfe87d1386ff7d444a365e9e
Joss Mackison <joss@thinkmill.com.au>
2016-08-19 10:21:41 +1000

    Allow `fill` to be set on SVG icons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,15 +1,23 @@
 import React, { PropTypes } from 'react';
 import icons from '../icons';
 
-const Icon = ({ type, ...props }) => (
-	<span
-		dangerouslySetInnerHTML={{ __html: icons[type] }}
-		{...props}
-	/>
-);
+const Icon = ({ color, type, ...props }) => {
+	const icon = icons[type];
+
+	return (
+		<span
+			dangerouslySetInnerHTML={{ __html: icon(color) }}
+			{...props}
+		/>
+	);
+};
 
 Icon.propTypes = {
+	color: PropTypes.string,
 	type: PropTypes.oneOf(Object.keys(icons)),
+};
+Icon.defaultProps = {
+	color: 'white',
 };
 
 export default Icon;
