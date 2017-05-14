0820f0301b8fc73fc08cb59981d56f2c7adaa049
islam3zzat <islam3zzat@gmail.com>
2017-05-14 10:47:06 +0200

    fixing linitng errors

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,59 +1,51 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 
 export default class ImageItem extends Component {
-	
-	constructor(props) {
+	constructor (props) {
 		super(props);
-
 		// initiaing state
 		this.state = {
 			imageSource: props.initialImage || this.props.src,
-			width: props.initialImage?'100vw':this.props.style.width,
-			srcset: props.initialImage?null:this.props.srcset,
-		}
-
+			width: props.initialImage ? '100vw' : this.props.style.width,
+			srcset: props.initialImage ? null : this.props.srcset,
+		};
 	}
 
-	componentDidMount() {
+	componentDidMount () {
 		// creating image object
 		this.lightboxImage = new window.Image();
 		// setting display image
-		if(this.props.initialImage) this.setDisplayImage(this.props);
+		if (this.props.initialImage) this.setDisplayImage(this.props);
 	}
 	componentWillReceiveProps (nextProps) {
 		this.setState({
-			imageSource: nextProps.initialImage ||  nextProps.src,
-			width: nextProps.initialImage?'100vw': nextProps.style.width,
-			srcset: nextProps.initialImage?null: nextProps.srcset,
+			imageSource: nextProps.initialImage || nextProps.src,
+			width: nextProps.initialImage ? '100vw' : nextProps.style.width,
+			srcset: nextProps.initialImage ? null : nextProps.srcset,
 		});
-		console.dir(nextProps)
-		if(nextProps.initialImage)  this.setDisplayImage(nextProps)
+		if (nextProps.initialImage) this.setDisplayImage(nextProps);
 	}
-	componentWillUnmount() {
+	componentWillUnmount () {
 		// removing image object
 		this.lightboxImage.onerror = null;
 		this.lightboxImage.onload = null;
 		this.lightboxImage = null;
 	}
-	setDisplayImage({src:image, srcset}) {
+	setDisplayImage ({ src: image, srcset }) {
 		// feeding the img object the main image
-		this.lightboxImage.src = image
+		this.lightboxImage.src = image;
 		// getting passed width
-		const width = this.props.style.width
-
-
-		//  initializing new state
+		const width = this.props.style.width;
+		// initializing new state
 		const newState = {
-				imageSource: image,
-				width,
-			}
-		
+			imageSource: image,
+			width,
+		};
 		// if we have srcset we'll pass it
-		if(srcset){
-			newState.srcset = srcset
+		if (srcset) {
+			newState.srcset = srcset;
 		}
-
 		// TODO getting fallback in case of errors
 		this.lightboxImage.onload = () => {
 			// updating image when image is available
@@ -62,27 +54,27 @@ export default class ImageItem extends Component {
 		};
 	}
 
-	render(){
+	render () {
 		// getting image props
 
 		const width = this.state.width;
-		const imageProps ={
-				alt:this.props.alt,
-				className:this.props.className,
-				onClick:this.props.onClick,
-				sizes:this.props.sizes,
-				style:({...this.props.style, width}),
-			    src:this.state.imageSource		    
-		}
+		const imageProps = {
+			alt: this.props.alt,
+			className: this.props.className,
+			onClick: this.props.onClick,
+			sizes: this.props.sizes,
+			style: ({ ...this.props.style, width }),
+			src: this.state.imageSource,
+		};
 		// put back srclist
-		if(this.state.srcSet){
-			imageProps.srcSet = this.state.srcSet
+		if (this.state.srcSet) {
+			imageProps.srcSet = this.state.srcSet;
 		}
 		// rendering image
 		return (
-				<img
+			<img
 				{...imageProps}
-			    />
+			/>
 		);
 	}
 }
