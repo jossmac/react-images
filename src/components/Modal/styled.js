aa479ea3812eff2db371fd33fff494a77c824f93
thedavies316 <devin.m.davies@gmail.com>
2019-06-17 00:09:35 -0400

    fixed zIndex for blanketCSS and positionerCSS

--- src/components/Modal/styled.js
+++ src/components/Modal/styled.js
@@ -23,7 +23,7 @@ export const blanketCSS = ({ isFullscreen }: BlanketState) => ({
   position: 'fixed',
   right: 0,
   top: 0,
-  zIndex: 1,
+  zIndex: 1199,
 });
 
 export const Blanket = (props: BlanketProps) => {
@@ -57,7 +57,7 @@ export const positionerCSS = () => ({
   position: 'fixed',
   right: 0,
   top: 0,
-  zIndex: 1,
+  zIndex: 1200,
 });
 
 export const Positioner = (props: PositionerProps) => {
