c4772f6545a44d5566a7f710e8ce61e96aa35a7e
David Wheatley <hi@davwheat.dev>
2021-02-23 15:26:15 +0000

    Fix build scripts on Windows

--- package-scripts.js
+++ package-scripts.js
@@ -10,13 +10,13 @@ module.exports = {
     build: {
       description: 'clean dist directory and run all builds',
       default: series(rimraf('dist'), rimraf('lib'), concurrent.nps('build.rollup', 'build.babel')),
-      rollup: 'rollup --config',
-      babel: 'babel src -d lib',
-      watch: 'babel src -d lib -w',
+      rollup: 'cross-env rollup --config',
+      babel: 'cross-env babel src -d lib',
+      watch: 'cross-env babel src -d lib -w',
       docs: series(rimraf('docs/dist'), 'webpack --progress -p'),
     },
     publish: {
-      default: series('nps build.docs', 'gh-pages -d docs/dist', rimraf('docs/dist')),
+      default: series('nps build.docs', 'cross-env gh-pages -d docs/dist', rimraf('docs/dist')),
     },
   },
 }
