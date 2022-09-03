83cacb80e9b003456a0d93e39d71c446420b2840
Danny Allen <me@dannya.com>
2022-09-03 23:55:13 +0100

    Remove unused functions

--- src/styles.js
+++ src/styles.js
@@ -13,8 +13,6 @@ import { blanketCSS, dialogCSS, positionerCSS } from './components/Modal/styled'
 type Props = { [key: string]: any }
 type StyleDef = Props => Object
 
-export type GetStyles = (string, Props) => {}
-
 export type CarouselStyles = {
   container: StyleDef,
   footer: StyleDef,
@@ -53,25 +51,4 @@ export const defaultModalStyles: CarouselStyles = {
   blanket: blanketCSS,
   dialog: dialogCSS,
   positioner: positionerCSS,
-}
-
-// Merge Utility
-// Allows consumers to extend a base Carousel or Modal with additional styles
-
-export function mergeStyles(source: Object, target: Object = {}) {
-  // initialize with source styles
-  const styles = { ...source }
-
-  // massage in target styles
-  Object.keys(target).forEach(key => {
-    if (source[key]) {
-      styles[key] = (rsCss, props) => {
-        return target[key](source[key](rsCss, props), props)
-      }
-    } else {
-      styles[key] = target[key]
-    }
-  })
-
-  return styles
 }
